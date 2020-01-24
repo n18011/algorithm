88e93b047816b9f9228b0fe955f0d67677653d9e
n18011 <n18011@std.it-college.ac.jp>
2020-01-24 11:45:04 +0900

    hello selection sort

---
+++ selection-sort/target/debug/deps/selection_sort-036c97ff294d7a46.d
@@ -0,0 +1,5 @@
+/home/n18011/sortAlgorithm/selection-sort/target/debug/deps/selection_sort-036c97ff294d7a46: src/main.rs
+
+/home/n18011/sortAlgorithm/selection-sort/target/debug/deps/selection_sort-036c97ff294d7a46.d: src/main.rs
+
+src/main.rs:
---
+++ selection-sort/target/debug/selection-sort.d
@@ -0,0 +1,1 @@
+/home/n18011/sortAlgorithm/selection-sort/target/debug/selection-sort: /home/n18011/sortAlgorithm/selection-sort/src/main.rs
