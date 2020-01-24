2cef21c81c41114a2e528122a12e37b76ee20c54
n18011 <n18011@std.it-college.ac.jp>
2020-01-24 22:04:34 +0900

    hello linear search

---
+++ linear-search/target/debug/deps/linear_search-9cb00b4ccfab798c.d
@@ -0,0 +1,5 @@
+/home/n18011/algorithm/linear-search/target/debug/deps/linear_search-9cb00b4ccfab798c: src/main.rs
+
+/home/n18011/algorithm/linear-search/target/debug/deps/linear_search-9cb00b4ccfab798c.d: src/main.rs
+
+src/main.rs:
---
+++ linear-search/target/debug/linear-search.d
@@ -0,0 +1,1 @@
+/home/n18011/algorithm/linear-search/target/debug/linear-search: /home/n18011/algorithm/linear-search/src/main.rs
