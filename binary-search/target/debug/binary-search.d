eaaa3480c9785016b96ad3f86d8ef95debea1e68
n18011 <n18011@std.it-college.ac.jp>
2020-01-25 17:25:05 +0900

    hello binary search

---
+++ binary-search/target/debug/binary-search.d
@@ -0,0 +1,1 @@
+/home/n18011/algorithm/binary-search/target/debug/binary-search: /home/n18011/algorithm/binary-search/src/main.rs
---
+++ binary-search/target/debug/deps/binary_search-d49e8ae0975a6469.d
@@ -0,0 +1,5 @@
+/home/n18011/algorithm/binary-search/target/debug/deps/binary_search-d49e8ae0975a6469: src/main.rs
+
+/home/n18011/algorithm/binary-search/target/debug/deps/binary_search-d49e8ae0975a6469.d: src/main.rs
+
+src/main.rs:
