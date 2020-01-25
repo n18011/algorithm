866d64c7a03273a2491a895c7f0a067fa9b47f63
n18011 <n18011@std.it-college.ac.jp>
2020-01-25 14:23:10 +0900

    hello sentinel search

---
+++ sentinel-search/target/debug/deps/sentinel_search-1522932445ab89b4.d
@@ -0,0 +1,5 @@
+/home/n18011/algorithm/sentinel-search/target/debug/deps/sentinel_search-1522932445ab89b4: src/main.rs
+
+/home/n18011/algorithm/sentinel-search/target/debug/deps/sentinel_search-1522932445ab89b4.d: src/main.rs
+
+src/main.rs:
---
+++ sentinel-search/target/debug/sentinel-search.d
@@ -0,0 +1,1 @@
+/home/n18011/algorithm/sentinel-search/target/debug/sentinel-search: /home/n18011/algorithm/sentinel-search/src/main.rs
