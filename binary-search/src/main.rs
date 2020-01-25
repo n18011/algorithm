e8fb9a95caa2cb7194393c6d8d8ed16fa37385e6
n18011 <n18011@std.it-college.ac.jp>
2020-01-25 18:22:05 +0900

    目的の値が0のときの挙動 デバック途中

--- binary-search/src/main.rs
+++ binary-search/src/main.rs
@@ -1,3 +1,28 @@
+// 二分探索法
 fn main() {
-    println!("Hello, world!");
+    // 探索したい元の配列(ベクタ)*昇順ソート済み
+    let v = vec![1, 3, 5, 7, 9];
+    // 配列ををコンソール出力
+    println!("{:?}", v);
+
+    // 目的の値
+    let x = 0;
+
+    let mut low = 0;
+    let mut high = v.len() - 1;
+
+    loop {
+        if low > high {
+            println!("xは存在しない");
+            break;
+        }
+        let k = (low + high) / 2;
+        println!("k is {}", k);
+        if v[k] == x {
+            println!("xは存在する: index is {}", k);
+            break;
+        }
+        if v[k] < x {low = k + 1;}
+        if v[k] > x {high = k - 1;}
+    }
 }
