c37ff959540786e8c0ecf42606bc4c90e01b7065
n18011 <n18011@std.it-college.ac.jp>
2020-05-24 22:08:58 +0900

    intial

---
+++ quick-sort/target/debug/deps/quick_sort-85008d2c0f38b6ba.d
@@ -0,0 +1,5 @@
+/home/n18011/algorithm/quick-sort/target/debug/deps/quick_sort-85008d2c0f38b6ba.rmeta: src/main.rs
+
+/home/n18011/algorithm/quick-sort/target/debug/deps/quick_sort-85008d2c0f38b6ba.d: src/main.rs
+
+src/main.rs:
---
+++ quick-sort/target/debug/deps/quick_sort-8a539e005b6a43d0.d
@@ -0,0 +1,5 @@
+/home/n18011/algorithm/quick-sort/target/debug/deps/quick_sort-8a539e005b6a43d0.rmeta: src/main.rs
+
+/home/n18011/algorithm/quick-sort/target/debug/deps/quick_sort-8a539e005b6a43d0.d: src/main.rs
+
+src/main.rs:
