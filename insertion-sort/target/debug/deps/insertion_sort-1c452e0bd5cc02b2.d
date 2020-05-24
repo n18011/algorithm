5eed5409460f4cdc684905499cbef8bfd23bebc6
n18011 <n18011@std.it-college.ac.jp>
2020-05-24 22:04:30 +0900

    c

---
+++ insertion-sort/target/debug/deps/insertion_sort-1c452e0bd5cc02b2.d
@@ -0,0 +1,5 @@
+/home/n18011/algorithm/insertion-sort/target/debug/deps/insertion_sort-1c452e0bd5cc02b2.rmeta: src/main.rs
+
+/home/n18011/algorithm/insertion-sort/target/debug/deps/insertion_sort-1c452e0bd5cc02b2.d: src/main.rs
+
+src/main.rs:
---
+++ insertion-sort/target/debug/deps/insertion_sort-6d7a5b2daa30430d.d
@@ -0,0 +1,5 @@
+/home/n18011/algorithm/insertion-sort/target/debug/deps/insertion_sort-6d7a5b2daa30430d.rmeta: src/main.rs
+
+/home/n18011/algorithm/insertion-sort/target/debug/deps/insertion_sort-6d7a5b2daa30430d.d: src/main.rs
+
+src/main.rs:
